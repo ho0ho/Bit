33dec972fba6e8c3e7c9e88de2163f68fcb955de
coco <zoross_@naver.com>
2019-04-16 10:46:20 +0900

    Add: 연산자 오버로딩(operator ==) - 전역함수(gettor), 멤버함수

--- 0416/am.cpp
+++ 0416/am.cpp
@@ -27,18 +27,47 @@ public:
 	void hohoh() { cout << "hohoho " << val << endl; }	// dynamic_cast(down-casting)
 };
 
+//int main() {
+//	Bird *e = new Eagle;
+//	Bird *s = new Seagull;
+//
+//	Bird *arr[] = { e, s };
+//	for (Bird *b : arr)
+//		b->fly();
+//
+//	Eagle *ee = dynamic_cast<Eagle *>(e);
+//	ee->hahah();
+//	Seagull *ss = dynamic_cast<Seagull *>(s);
+//	ss->hohoh();
+//
+//	getchar();
+//	return 0;
+//}
+
+class Number {
+	int fir;
+	int sec;
+public:
+	Number(int f = 0, int s = 0) :fir(f), sec(s) {}
+	~Number() {}
+
+	Number(const Number& cp) { fir = cp.fir; sec = cp.sec; }
+
+	Number& operator = (const Number &right) { fir = right.fir; sec = right.sec; return *this; }
+	bool operator == (int num) { return (fir == num); }		// overloading(member method)
+
+	void settor(int f, int s) { fir = f; sec = s; }
+	int gettor() const { return fir; }
+};
+
+bool operator == (const Number& n1, const Number& n2) {		// overloading(global function)
+	return n1.gettor() == n2.gettor();
+}
+
 int main() {
-	Bird *e = new Eagle;
-	Bird *s = new Seagull;
-
-	Bird *arr[] = { e, s };
-	for (Bird *b : arr)
-		b->fly();
-
-	Eagle *ee = dynamic_cast<Eagle *>(e);
-	ee->hahah();
-	Seagull *ss = dynamic_cast<Seagull *>(s);
-	ss->hohoh();
+	Number a(10, 20), b(20, 40), c(a);			// copy constructor
+	if (a == c) cout << "true" << endl;
+	else cout << "false" << endl;
 
 	getchar();
 	return 0;
