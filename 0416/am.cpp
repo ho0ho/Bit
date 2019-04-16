a9088d51169d4d56d84df3cceafce7324ad4ee90
coco <zoross_@naver.com>
2019-04-16 13:31:48 +0900

    Add: 연산자 오버로딩(<<) Edit: 연산자 오버로딩(+) 리턴 수정(const)

--- 0416/am.cpp
+++ 0416/am.cpp
@@ -47,19 +47,24 @@ public:
 class Number {
 	int fir;
 	int sec;
+	int *arr;
+	int size;
 public:
-	Number(int f = 0, int s = 0) :fir(f), sec(s) {}
+	Number(int f = 0, int s = 0) :fir(f), sec(s), size(10), arr(new int[size]) {}
 	~Number() {}
 
 	Number(const Number& cp) { fir = cp.fir; sec = cp.sec; }
 
 	Number& operator = (const Number &right) { fir = right.fir; sec = right.sec; return *this; }
 	bool operator == (const Number& right) { return (fir == right.fir && sec == right.sec); }		// overloading(member method)
-	Number& operator + (int num) {
-		fir += num;
-		sec += num;
-		return *this;
+	int& operator [] (int idx) { return arr[idx]; }			// [] 연산자 오버로딩 => 멤버로만 정의가능
+	int& operator [] (int idx) const { return arr[idx]; }	// ?
+	const Number operator + (int num) const {		// a + 2 = b; 이렇게 사용X (-> const리턴)
+		/*number n(fir + num, sec + num);*/				// 임시객체 n을 사용하므로 복사해서 넘겨야 함(&없이 리턴)
+		/*return n;*/
+		return Number(fir + num, sec + num);
 	}
+	operator int() const { return fir; }		// int() 형변환 연산자 오버로딩(리턴 없고 void 인자) => 멤버로만 정의가능
 
 	friend bool operator == (const Number& n1, const Number& n2);	// overloading(friend function)		
 	friend Number& operator + (int num, Number& n);
@@ -81,6 +86,11 @@ Number& operator + (int num, Number& n) {		// 교환법칙까지 만족하게 하기 위한 전
 	return n;
 }
 
+ostream& operator << (ostream& out, const Number& obj) {		// << 연산자는 전역함수로 오버로딩(멤버함수하려면 cout클래스의 멤버로 정의했어야함)
+	out << "(" << obj.gettor_fir() << ", " << obj.gettor_sec() << ")" << endl;
+	return out;
+}
+
 int main() {
 	Number a(10, 20), b(20, 40), c(a);			// copy constructor
 	if (a == b) cout << "true" << endl;
@@ -91,6 +101,13 @@ int main() {
 
 	a = 5 + a;			// 전역함수 operator + () 호출
 	cout << a.gettor_fir() << ", " << a.gettor_sec() << endl;
+
+	cout << int(a) << endl;
+
+	b[0] = 3;
+	cout << b[0] << endl;
+
+	cout << a;
 
 	getchar();
 	return 0;
